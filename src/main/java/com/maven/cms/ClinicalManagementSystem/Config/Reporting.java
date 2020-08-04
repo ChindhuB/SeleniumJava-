8636dfce0631959907a364aba4861b49c2affcab
ChindhuB <chindhubabu@gmail.com>
2020-08-04 02:15:52 -0400

    Commit

--- src/main/java/com/maven/cms/ClinicalManagementSystem/Config/Reporting.java
+++ src/main/java/com/maven/cms/ClinicalManagementSystem/Config/Reporting.java
@@ -2,6 +2,7 @@ package com.maven.cms.ClinicalManagementSystem.Config;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import org.testng.ITestContext;
@@ -15,9 +16,12 @@ import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.markuputils.ExtentColor;
 import com.aventstack.extentreports.markuputils.MarkupHelper;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
 
 public class Reporting extends TestListenerAdapter {
-	public ExtentSparkReporter htmlReporter;
+	public ExtentHtmlReporter htmlReporter;
 	ExtentReports extent;
 	ExtentTest logger;
 
@@ -25,33 +29,51 @@ public class Reporting extends TestListenerAdapter {
 		String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());// time stamp
 		// specify name and location of the report
 		String repName = System.getProperty("user.dir") + "/test-output/" + "Test-Report-" + timeStamp + ".html";
-		htmlReporter = new ExtentSparkReporter(repName);
-		//htmlReporter.loadXMLConfig(System.getProperty("user.dir") + "/extent-config.xml");
+		htmlReporter = new ExtentHtmlReporter (repName);
+		htmlReporter.loadXMLConfig(System.getProperty("user.dir") + "/extent-config.xml");
 
 		extent = new ExtentReports();
 		extent.attachReporter(htmlReporter);
 		extent.setSystemInfo("Host name", "localhost");
 		extent.setSystemInfo("Environemnt", "QA");
 		extent.setSystemInfo("user", "Chindhu");
+		extent.setReportUsesManualConfiguration(true);
 
-		htmlReporter.config().setDocumentTitle("Clinical Managment System"); // Title of report
+
+		htmlReporter.config().setDocumentTitle("Sample Project"); // Title of report
 		htmlReporter.config().setReportName("Functional Test Report"); // name of the report
+		htmlReporter.config().setTheme(Theme.STANDARD);
+		
+		
 
 	}
 
-	public void onTestSuccess(ITestResult tr) {
+	public void onTestStart(ITestResult tr) {
 		// create new entry in the report
-		logger = extent.createTest(tr.getName());
+		String qualifiedName = tr.getTestClass().getName();
+		int last = qualifiedName.lastIndexOf(".");
+		String className = qualifiedName.substring(last+1);
+		logger = extent.createTest(className, tr.getMethod().getDescription());
+		logger.assignCategory(tr.getTestContext().getSuite().getName());
+		logger.getModel().setStartTime(getTime(tr.getStartMillis()));
+	}
+
+	public void onTestSuccess(ITestResult tr) {
 		// send the passed information to the report with GREEN color highlighted
 		logger.log(Status.PASS, MarkupHelper.createLabel(tr.getName(), ExtentColor.GREEN));
+		logger.getModel().setEndTime(getTime(tr.getEndMillis()));
 
 	}
 
 	public void onTestFailure(ITestResult tr) {
-		// create new entry in the report
-		logger = extent.createTest(tr.getName());
 		// send the failed information to the report with RED color highlighted
 		logger.log(Status.FAIL, MarkupHelper.createLabel(tr.getName(), ExtentColor.RED));
+		logger.getModel().setEndTime(getTime(tr.getEndMillis()));
+		try {
+			logger.log(Status.FAIL, tr.getThrowable());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		String screenshotpath = System.getProperty("user.dir") + "/src/test/Screenshots/" + tr.getName() + ".png";
 		File screenshot = new File(screenshotpath);
 		if (screenshot.exists()) {
@@ -64,11 +86,21 @@ public class Reporting extends TestListenerAdapter {
 	}
 
 	public void onTestSkipped(ITestResult tr) {
-		// create new entry in the report
-		logger = extent.createTest(tr.getName());
 		// send the skipped information to the report with ORANGE color highlighted
 		logger.log(Status.SKIP, MarkupHelper.createLabel(tr.getName(), ExtentColor.ORANGE));
+		logger.getModel().setEndTime(getTime(tr.getEndMillis()));
+		try {
+			logger.log(Status.SKIP, tr.getThrowable());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
+	}
+
+	private Date getTime(long millis) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTimeInMillis(millis);
+		return calendar.getTime();
 	}
 
 	public void onFinish(ITestContext testContext) {
