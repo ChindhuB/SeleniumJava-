d717ba919767ba9e5ae3017c53535cb1c7e419c7
ChindhuB <chindhubabu@gmail.com>
2020-09-06 14:32:34 -0400

    Commit

--- run.bat
+++ run.bat
@@ -3,8 +3,9 @@ echo Hello this a test batch file
 pause
 dir c:\windows
 pause
-cd /Users/Cibin/Desktop/BusyQA/Eclipse/sampleClinicalManagementSystem
+cd /Users/Cibin/Desktop/BusyQA/Eclipse/ClinicalManagementSystem
 git add --all
 git commit -m "Commit"
-git push origin master
+git push origin ClinicalManagementSystem
 pause
+
